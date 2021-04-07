af89a8cd07737383a57dc054892e804825fc247b
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-04-06 17:04:53 -0700

    add port var

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { type } = require("os");
 const { animals } = require("./data/animals.json");
+const PORT = process.env.PORT || 3001;
 
 // instantiate the server
 const app = express();
@@ -65,6 +66,6 @@ app.get("/api/animals", (req, res) => {
 });
 
 // make server listen
-app.listen(3001, () => {
-  console.log("API server now on port 3001!");
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
